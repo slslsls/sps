4820fd44e08e75fee65dfaa1b27a263660348b93
Spencer Scarpelli <spencerscarpelli@gmail.com>
2017-09-03 15:18:17 -0700

    add lint config, company_list_service file and unit test file

---
+++ services/company_list_service.js
@@ -0,0 +1,18 @@
+const config = require('../config');
+const rp = require('rp');
+
+function buildSymbolsRequest(exchange) {
+
+}
+
+function getMainstreamSymbols() {
+
+}
+
+function getOtcSymbols() {
+
+}
+
+module.exports = {
+  getMainstreamSymbols
+}
---
+++ test/company_list_service_test.js
@@ -0,0 +1,9 @@
+const mocha = require('mocha');
+const chai = require('chai');
+const sinon = require('sinon');
+const { expect } = chai;
+chai.use(require('sinon-chai'));
+
+describe('companyListService', () => {
+  describe
+})
