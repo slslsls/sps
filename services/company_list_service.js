2279268edb87b6a7772158b39044c9a2335cf530
Spencer Scarpelli <spencerscarpelli@gmail.com>
2017-09-03 23:31:07 -0700

    write buildRequestOptions function; fix some lint errors

--- services/company_list_service.js
+++ services/company_list_service.js
@@ -1,8 +1,19 @@
+'use strict';
+
 const config = require('../config.json');
 const rp = require('request-promise');
 
 function buildRequestOptions(exchange) {
-
+  if (!config.serviceLinks.symbols[exchange]) {
+    throw ReferenceError('Invalid exchange specified');
+  }
+  return {
+    uri: config.serviceLinks.symbols[exchange],
+    json: true,
+    headers: {
+      'User-Agent': 'Request-Promise'
+    }
+  }
 }
 
 function getMainstreamSymbols() {
--- test/company_list_service_test.js
+++ test/company_list_service_test.js
@@ -1,11 +1,11 @@
+'use strict';
+
 const config = require('../config.json');
-const otcUri = config.serviceLinks.otcSymbols;
 const _ = require('lodash');
 const companyListService = require('../services/company_list_service');
 const { buildRequestOptions } = companyListService;
 const { getMainstreamSymbols } = companyListService;
 const { getOtcSymbols } = companyListService;
-const mocha = require('mocha');
 const chai = require('chai');
 const sinon = require('sinon');
 const { expect } = chai;
@@ -15,28 +15,32 @@ const exchanges = [
   'amex',
   'otc'
 ];
+
 chai.use(require('sinon-chai'));
 
 describe('companyListService', () => {
   describe('buildRequestOptions', () => {
     _.each(exchanges, exchange => {
       it(`should return an options object with ${exchange} uri`, () => {
-        requestOptions = buildRequestOptions(exchange);
+        const requestOptions = buildRequestOptions(exchange);
         expect(requestOptions.uri).to.equal(config.serviceLinks.symbols[exchange]);
       });
     });
     it('should set options.json to true', () => {
-      requestOptions = buildRequestOptions('nasdaq');
+      const requestOptions = buildRequestOptions('nasdaq');
       expect(requestOptions.json).to.equal(true);
     });
     it('should set User-Agent header to Request-Promise', () => {
-      requestOptions = buildRequestOptions('nasdaq');
+      const requestOptions = buildRequestOptions('nasdaq');
       expect(requestOptions).to.have.property('headers');
       expect(requestOptions.headers).to.have.property('User-Agent');
       expect(requestOptions.headers['User-Agent']).to.equal('Request-Promise');
     });
     it('should throw an error if specified exchange is not in config', () => {
-      expect(buildRequestOptions('not-a-valid-exchange')).to.throw();
+      function wrappedFn() {
+        return buildRequestOptions('not-a-valid-exchange');
+      }
+      expect(wrappedFn).to.throw(ReferenceError);
     });
   });
   describe('getMainstreamSymbols', () => {
